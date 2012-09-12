398078b209f54de33f9d8d4555835b87a46843f0
Eli White <eli@eliw.com>
2012-09-12 13:30:44 -0400

    Fixes #7 - Augments Cookie to allow for secure & httponly cookies

--- framework/cookie.php
+++ framework/cookie.php
@@ -20,27 +20,36 @@ class Cookie
      * # Handles setting domain for you
      * # Defaults the path to '/' instead of 'current directory'
      * # If $expire is less than time(), it assumes you want to add the value to time()
+     * # Defaults the 'httponly' protection to be on.
      *
      * @author Eli White <eli@eliw.com>
-     * @param mixed  Documentation
+     * @param string $name Name for the cookie (will be prefixed by any cookie prefix)
+     * @param mixed $value The value you wish to set
+     * @param integer $exp How long until this cookie expires (seconds), or a timestamp.
+     * @param string $path The path this cookie will be available from (default '/')
+     * @param boolean $sec Is this for secure connections only? (default false)
+     * @param boolean $http Should this be for http connections only? (default true)
      * @return void
      * @access public
      **/
-    public static function set($name, $value, $expire = 0, $path = '/')
+    public static function set($name, $value, $exp = 0, $path = '/', $sec = false, $http = true)
     {
+        // Prep our config:
+        $config = config();
+        
         // Determine the final expiration
         $now = time();
-        if ($expire && ($expire < $now)) {
-            $expire += $now;
+        if ($exp && ($exp < $now)) {
+            $exp += $now;
         }
 
         // Add in the prefix if it exists:
-        if ($prefix = config()->cookies->prefix) {
+        if ($prefix = $config->cookies->prefix) {
             $name = "{$prefix}_{$name}";
         }
 
         // Now we can just set the cookie:
-        setcookie($name, $value, $expire, $path, (string)config()->env->domain);
+        setcookie($name, $value, $exp, $path, (string)$config->env->domain, $sec, $http);
     }
 
     /**
@@ -50,15 +59,18 @@ class Cookie
      *  how cookies work.
      *
      * @author Eli White <eli@eliw.com>
-     * @param mixed  Documentation
+     * @param string $name Name for the cookie
+     * @param string $path The path this cookie will be available from (default '/')
+     * @param boolean $sec Is this for secure connections only? (default false)
+     * @param boolean $http Should this be for http connections only? (default true)
      * @return void
      * @access public
      **/
-    public static function delete($name, $path = '/')
+    public static function delete($name, $path = '/', $sec = false, $http = true)
     {
         // Cheating.  All the logic we need is in 'set', so why bother repeating it:
         //  Set the cookie to expire 2 days ago (why 2 days?  why not?)
-        self::set($name, 0, -172800, $path);
+        self::set($name, 0, -172800, $path, $sec, $http);
     }
 
     /**
